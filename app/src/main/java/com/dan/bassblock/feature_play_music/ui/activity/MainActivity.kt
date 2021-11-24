2059cc8de8281190655b7c1d0c30c3f8e45991a6
Dan Sabau <danioansabau@gmail.com>
2021-11-24 22:38:36 +0200

    structure setup

--- app/src/main/java/com/dan/bassblock/feature_play_music/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dan/bassblock/feature_play_music/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dan.bassblock
+package com.dan.bassblock.feature_play_music.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dan.bassblock.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
