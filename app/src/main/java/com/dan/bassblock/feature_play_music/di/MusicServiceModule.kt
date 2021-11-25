d52ae44f381a9334e16c66109cf582fc7605e627
Dan Sabau <danioansabau@gmail.com>
2021-11-25 10:56:00 +0200

    dagger_hilt setup

---
+++ app/src/main/java/com/dan/bassblock/feature_play_music/di/MusicServiceModule.kt
@@ -0,0 +1,41 @@
+package com.dan.bassblock.feature_play_music.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+class MusicServiceModule {
+
+    @Provides
+    @ServiceScoped
+    fun provideAudioAttributes(): AudioAttributes = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @Provides
+    @ServiceScoped
+    fun provideExoplayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = ExoPlayer.Builder(context).build().apply {
+        this.setAudioAttributes(audioAttributes, true)
+        this.setHandleAudioBecomingNoisy(true)
+    }
+
+    @Provides
+    @ServiceScoped
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSource.Factory(context)
+}
--- app/src/main/java/com/dan/bassblock/feature_play_music/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dan/bassblock/feature_play_music/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package com.dan.bassblock.feature_play_music.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
 import com.dan.bassblock.R
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
